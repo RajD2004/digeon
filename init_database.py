a89af3ec3151462e927c8c0c6b0dcf9a715dd1f0
Sairaj Goutham Dhullipalla <dhullipa@msu.edu>
2025-08-30 17:56:26 -0400

    RDS AND EC2 fix 8/30/2025 5:35 pm

--- init_database.py
+++ init_database.py
@@ -10,7 +10,7 @@ excel_folder = "excel"
 conn = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
+    password=os.getenv("DB_PASS"),
     database=os.getenv("DB_NAME"),
     port=int(os.getenv("DB_PORT", 3306)),
 )
